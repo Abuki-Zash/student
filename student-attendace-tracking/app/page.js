23790bffdbd43162e1338350fd66b92ab47793ee
--replace-all <abukizash@gmail.com>
2025-11-07 08:20:26 +0300

    student-attendace-system first commit

---
+++ student-attendace-tracking/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div>
+        <h2>Hello React</h2>
+        <br></br>
+      </div>
+      <Button>click</Button>
+    </div>
+  );
+}
