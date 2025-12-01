960e2f84a44b20625bb203f450c3f15a09dd057a
--replace-all <abukizash@gmail.com>
2025-12-01 11:02:51 +0300

    add dark mode using shadcn qucick and easy

--- student-attendace-tracking/app/dashboard/page.js
+++ student-attendace-tracking/app/dashboard/page.js
@@ -1,7 +1,12 @@
 "use client";
 import React from "react";
-
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
 function Dashboard() {
+  const { setTheme } = useTheme();
+  useEffect(() => {
+    setTheme("dark");
+  });
   return <div>page</div>;
 }
 
